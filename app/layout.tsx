c467a8d6b8c6d256bce70fb7084cfb3ea9ba80b4
RZ <tecegroup@gmail.com>
2024-01-13 21:48:50 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import { Providers } from "./providers";
 import { Metadata } from "next";
 import { cn } from "@/lib/utils";
 
-const title =
-  "Platforms Starter Kit – The all-in-one starter kit for building multi-tenant applications.";
+ "Platforms Starter Kit – The all-in-one starter kit for building multi-tenant applications.";
 const description =
   "The Platforms Starter Kit is a full-stack Next.js app with multi-tenancy and custom domain support. Built with Next.js App Router, Vercel Postgres and the Vercel Domains API.";
 const image = "https://vercel.pub/thumbnail.png";
